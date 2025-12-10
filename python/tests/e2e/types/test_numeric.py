8edee077171874dcb162ecaa53fd8d16fcc6d7e4
Patryk Czajka <patryk.czajka@snowflake.com>
2025-12-10 10:54:36 +0100

    review comments from tests

--- python/tests/e2e/types/test_numeric.py
+++ python/tests/e2e/types/test_numeric.py
@@ -5,7 +5,7 @@ def test_should_cast_number_and_its_synonyms_to_appropriate_type_and_preserve_va
     cursor,
 ):
     # Given Snowflake client is logged in
-    assert not cursor.connection.is_closed(), "Connection should be open"
+    assert cursor
 
     # When Query selecting literal values of NUMBER, DECIMAL, DEC, NUMERIC types is executed
     sql = """
@@ -36,57 +36,52 @@ def test_should_cast_number_and_its_synonyms_to_appropriate_type_and_preserve_va
 
 
 def test_should_cast_number_and_its_synonyms_to_appropriate_type_and_preserve_values_when_selecting_from_table(
-    cursor,
+    cursor, tmp_schema
 ):
-    """Test SELECT FROM TABLE with NUMBER, DECIMAL, DEC, NUMERIC types."""
     # Given Snowflake client is logged in
-    table_name = "test_number_select_from_table"
-    assert not cursor.connection.is_closed(), "Connection should be open"
-    try:
-        # And A table with columns of types NUMBER, DECIMAL, DEC, NUMERIC is created
+    table_name = f"{tmp_schema}.test_number_select_from_table"
+    assert cursor, "Cursor should be open"
+    # And A table with columns of types NUMBER, DECIMAL, DEC, NUMERIC is created
 
-        # SQL weirdly split into lines as test validator had a stroke reading multiline strings
-        create_sql = (
-            f"CREATE OR REPLACE TEMPORARY TABLE {table_name}"
-            " (number_col NUMBER(10,0),"
-            " decimal_col DECIMAL(10,3),"
-            " dec_col DEC(20,6),"
-            " numeric_col NUMERIC(20,9))"
-        )
-        cursor.execute(create_sql)
-        # And Data is inserted into the table
-        insert_sql = f"INSERT INTO {table_name} VALUES (123456789, 123456.789, 123.0, 0.123456789)"
-        cursor.execute(insert_sql)
+    # SQL weirdly split into lines as test validator had a stroke reading multiline strings
+    create_sql = (
+        f"CREATE OR REPLACE TEMPORARY TABLE {table_name}"
+        " (number_col NUMBER(10,0),"
+        " decimal_col DECIMAL(10,3),"
+        " dec_col DEC(20,6),"
+        " numeric_col NUMERIC(20,9))"
+    )
+    cursor.execute(create_sql)
+    # And Data is inserted into the table
+    insert_sql = (
+        f"INSERT INTO {table_name} VALUES (123456789, 123456.789, 123.0, 0.123456789)"
+    )
+    cursor.execute(insert_sql)
 
-        # When Query selecting data from the table is executed
-        select_sql = f"SELECT * FROM {table_name}"
-        cursor.execute(select_sql)
-        values = cursor.fetchone()
+    # When Query selecting data from the table is executed
+    select_sql = f"SELECT * FROM {table_name}"
+    cursor.execute(select_sql)
+    values = cursor.fetchone()
 
-        # Then All returned values should be of appropriate type
-        expected_types = (int, Decimal, Decimal, Decimal)
-        for value, expected_type in zip(values, expected_types):
-            assert isinstance(
-                value, expected_type
-            ), f"Value should be Decimal, got {type(value)}"
-        # And All returned values should be equal to the inserted values
-        expected_values = (
-            int("123456789"),
-            Decimal("123456.789"),
-            Decimal("123.0"),
-            Decimal("0.123456789"),
-        )
-        assert values == expected_values, f"Expected {expected_values}, got {values}"
-    finally:
-        try:
-            cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
-        except Exception:
-            pass
+    # Then All returned values should be of appropriate type
+    expected_types = (int, Decimal, Decimal, Decimal)
+    for value, expected_type in zip(values, expected_types):
+        assert isinstance(
+            value, expected_type
+        ), f"Value should be Decimal, got {type(value)}"
+    # And All returned values should be equal to the inserted values
+    expected_values = (
+        int("123456789"),
+        Decimal("123456.789"),
+        Decimal("123.0"),
+        Decimal("0.123456789"),
+    )
+    assert values == expected_values, f"Expected {expected_values}, got {values}"
 
 
 def test_should_handle_maximum_precision_values_correctly(cursor):
     # Given Snowflake client is logged in
-    assert not cursor.connection.is_closed(), "Connection should be open"
+    assert cursor
 
     # When Query "SELECT 1.2345678901234567890123456789012345678::NUMBER(38,37) as max_precision_col" is executed
     # And Query "SELECT 99999999999999999999999999999999999999::NUMBER(38,0) as max_value_col" is executed
