80d7514f9b2f98b4669677671018f19bac722b0b
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2026-01-07 17:51:18 +0100

    Initial compatibility with snowflake.core

---
+++ python/src/snowflake/connector/telemetry.py
@@ -0,0 +1,16 @@
+"""BACKWARD COMPATIBILITY MODULE ONLY"""
+
+from enum import Enum
+
+
+class TelemetryData:
+    pass
+
+
+class TelemetryField(Enum):
+    KEY_SOURCE = "source"
+    KEY_TYPE = "type"
+
+
+class TelemetryClient:
+    pass
