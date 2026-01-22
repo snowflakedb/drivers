aaafa011d2cb7dec949c49b57e962d25962301d5
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2026-01-22 14:23:21 +0000

    SNOW-2467819: Extend key-pair tests

--- sf_core/tests/e2e/authentication/private_key_auth.rs
+++ sf_core/tests/e2e/authentication/private_key_auth.rs
@@ -1,5 +1,7 @@
 use crate::common::private_key_helper;
 use crate::common::snowflake_test_client::SnowflakeTestClient;
+use base64::{Engine as _, engine::general_purpose};
+use std::fs;
 
 #[test]
 fn should_authenticate_using_private_file_with_password() {
@@ -25,6 +27,53 @@ fn should_fail_jwt_authentication_when_invalid_private_key_provided() {
 
     //Then There is error returned
     client.assert_login_error(result);
+}
+
+#[test]
+fn should_authenticate_using_private_key_as_bytes() {
+    //Given Authentication is set to JWT and private key is provided as bytes
+    let mut client = SnowflakeTestClient::with_default_params();
+    client.set_connection_option("authenticator", "SNOWFLAKE_JWT");
+
+    let temp_key_file = private_key_helper::get_private_key_from_parameters(&client.parameters)
+        .expect("Failed to create private key file");
+
+    // Read the private key file and encode as base64
+    let key_contents = fs::read(temp_key_file.path()).expect("Failed to read private key file");
+    let key_base64 = general_purpose::STANDARD.encode(&key_contents);
+
+    client.set_connection_option("private_key", &key_base64);
+    if let Some(password) = &client.parameters.private_key_password {
+        client.set_connection_option("private_key_password", password);
+    }
+    client.set_temp_key_file(temp_key_file);
+
+    //When Trying to Connect
+    let result = client.connect();
+
+    //Then Login is successful and simple query can be executed
+    client.verify_simple_query(result);
+}
+
+#[test]
+fn should_automatically_update_authenticator_to_jwt_if_key_pair_params_present() {
+    //Given private key or private key file is provided and authenticator is not JWT
+    let mut client = SnowflakeTestClient::with_default_params();
+    // Note: NOT setting authenticator to SNOWFLAKE_JWT - it should be auto-detected
+
+    let temp_key_file = private_key_helper::get_private_key_from_parameters(&client.parameters)
+        .expect("Failed to create private key file");
+    client.set_connection_option("private_key_file", temp_key_file.path().to_str().unwrap());
+    if let Some(password) = &client.parameters.private_key_password {
+        client.set_connection_option("private_key_password", password);
+    }
+    client.set_temp_key_file(temp_key_file);
+
+    //When Trying to Connect
+    let result = client.connect();
+
+    //Then Connector changes authenticator to JWT and login is successful and simple query can be executed
+    client.verify_simple_query(result);
 }
 
 fn set_invalid_private_key_file(client: &mut SnowflakeTestClient) {
