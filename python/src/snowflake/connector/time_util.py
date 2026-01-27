b2f3581f957fbdef25674a9a362623642d56e713
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2026-01-27 18:08:33 +0100

    fixup! fixup! Initial compatibility with snowflake.core

--- python/src/snowflake/connector/time_util.py
+++ python/src/snowflake/connector/time_util.py
@@ -2,10 +2,10 @@
 
 import time
 
-from snowflake.connector._internal import internal
+from snowflake.connector._internal import internal_api
 
 
-@internal
+@internal_api
 def get_time_millis() -> int:
     """Return the current time in milliseconds."""
     return int(time.time() * 1000)
