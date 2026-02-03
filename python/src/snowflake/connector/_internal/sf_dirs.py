16870f117c9d5aa9ec5538ca8697e672c760f65c
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2026-02-03 10:12:03 +0100

    NO-SNOW: Compatbility with CLI

--- python/src/snowflake/connector/_internal/sf_dirs.py
+++ python/src/snowflake/connector/_internal/sf_dirs.py
@@ -61,3 +61,19 @@ class SFPlatformDirs:
     def user_config_path(self) -> pathlib.Path:
         """Data directory tied to to the user"""
         return self.single_dir
+
+
+# Snowflake's central platform dependent directories, if the folder
+# ~/.snowflake/ (customizable by the environment variable SNOWFLAKE_HOME) exists
+# we use that folder for everything. Otherwise, we fall back to platformdirs
+# defaults. Please see comments in sf_dir.py for more information.
+
+DIRS = _resolve_platform_dirs()
+
+# Snowflake's configuration files. By default, platformdirs will resolve
+# them to these places depending on OS:
+#   * Linux: `~/.config/snowflake/filename` but can be updated with XDG vars
+#   * Windows: `%USERPROFILE%\AppData\Local\snowflake\filename`
+#   * Mac: `~/Library/Application Support/snowflake/filename`
+CONNECTIONS_FILE = DIRS.user_config_path / "connections.toml"
+CONFIG_FILE = DIRS.user_config_path / "config.toml"
