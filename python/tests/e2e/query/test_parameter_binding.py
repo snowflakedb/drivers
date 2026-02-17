6951e00dec4d9495494ac3c4c1f045283a36bbd0
Patryk Czajka <patryk.czajka@snowflake.com>
2026-02-17 12:01:49 +0100

    fix reference test

--- python/tests/e2e/query/test_parameter_binding.py
+++ python/tests/e2e/query/test_parameter_binding.py
@@ -230,9 +230,18 @@ class TestEdgeCases:
 
         # When Query with 3 placeholders is executed with 1 argument
         # Then Error should be raised for too few arguments
-        from snowflake.connector._internal.protobuf_gen.proto_exception import ProtoApplicationException
+        from ...compatibility import IS_UNIVERSAL_DRIVER
 
-        with pytest.raises(ProtoApplicationException):
+        if IS_UNIVERSAL_DRIVER:
+            from snowflake.connector._internal.protobuf_gen.proto_exception import ProtoApplicationException
+
+            expected_error = ProtoApplicationException
+        else:
+            from snowflake.connector import DatabaseError
+
+            expected_error = DatabaseError
+
+        with pytest.raises(expected_error):
             cursor.execute("SELECT ?, ?, ?", (1,))
 
 
