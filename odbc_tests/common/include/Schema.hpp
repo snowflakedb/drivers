5f0127a8c115caecda8d4709688aeda90f7cfc54
Piotr Bulawa <piotr.bulawa@snowflake.com>
2026-03-19 22:43:50 +0100

    Revert diag changes

--- odbc_tests/common/include/Schema.hpp
+++ odbc_tests/common/include/Schema.hpp
@@ -10,21 +10,19 @@
 #include <string>
 
 #include "Connection.hpp"
-#include "get_diag_rec.hpp"
 #include "odbc_cast.hpp"
-#include "odbc_return_code.hpp"
 
 class Schema {
  public:
   Schema(Connection& conn, const std::string& schema_name)
       : execute_fn([&conn](const std::string& sql) { conn.execute(sql); }), schema_name(schema_name) {
-    create_schema_with_diagnostics(conn.handleWrapper().getHandle());
+    execute_fn("CREATE SCHEMA IF NOT EXISTS " + schema_name);
     execute_fn("USE SCHEMA " + schema_name);
   }
 
   Schema(const SQLHDBC dbc, const std::string& schema_name)
       : execute_fn(make_dbc_executor(dbc)), schema_name(schema_name) {
-    create_schema_with_diagnostics(dbc);
+    execute_fn("CREATE SCHEMA IF NOT EXISTS " + schema_name);
     execute_fn("USE SCHEMA " + schema_name);
   }
 
@@ -66,42 +64,6 @@ class Schema {
     std::random_device rd;
     std::mt19937_64 gen(rd());
     return "SCHEMA_" + std::to_string(gen());
-  }
-
-  void create_schema_with_diagnostics(SQLHANDLE dbc) {
-    const std::string sql = "CREATE SCHEMA IF NOT EXISTS " + schema_name;
-
-    SQLHSTMT stmt = SQL_NULL_HSTMT;
-    SQLRETURN ret = SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
-    if (!SQL_SUCCEEDED(ret)) {
-      WARN("[Schema] CREATE: SQLAllocHandle failed for " << schema_name);
-      execute_fn(sql);
-      return;
-    }
-
-    ret = SQLExecDirect(stmt, sqlchar(sql.c_str()), SQL_NTS);
-
-    if (!SQL_SUCCEEDED(ret)) {
-      auto diags = get_diag_rec(SQL_HANDLE_STMT, stmt);
-      SQLLEN row_count = -1;
-      SQLRowCount(stmt, &row_count);
-
-      WARN("[Schema] CREATE " << schema_name << " FAILED: ret=" << return_code_to_string(ret)
-                              << " row_count=" << row_count << " diag_count=" << diags.size());
-
-      for (size_t i = 0; i < diags.size(); ++i) {
-        WARN("[Schema]   diag[" << i << "] SQLSTATE=" << diags[i].sqlState << " NativeError=" << diags[i].nativeError
-                                << " " << diags[i].messageText);
-      }
-
-      SQLFreeStmt(stmt, SQL_CLOSE);
-      SQLFreeHandle(SQL_HANDLE_STMT, stmt);
-      execute_fn(sql);
-      return;
-    }
-
-    SQLFreeStmt(stmt, SQL_CLOSE);
-    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
   }
 
   static std::function<void(const std::string&)> make_dbc_executor(SQLHDBC dbc) {
