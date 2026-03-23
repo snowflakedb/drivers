aef5ce23f6a9552f7a44881c87c5277e258e0e27
Jakub Szczerbiński <jakub.szczerbinski@snowflake.com>
2026-03-23 11:56:52 +0100

    Trace based tests

---
+++ odbc_trace_tool/src/parser/unixodbc.rs
@@ -0,0 +1,23 @@
+use snafu::Location;
+use snafu::prelude::*;
+
+use crate::model::TraceLog;
+
+#[derive(Snafu, Debug)]
+pub enum UnixOdbcParserError {
+    #[snafu(display("unixODBC trace parser is not yet implemented"))]
+    NotImplemented {
+        #[snafu(implicit)]
+        location: Location,
+    },
+}
+
+type Result<T> = std::result::Result<T, UnixOdbcParserError>;
+
+pub fn parse_file(_path: &std::path::Path) -> Result<TraceLog> {
+    NotImplementedSnafu.fail()
+}
+
+pub fn parse_str(_content: &str) -> Result<TraceLog> {
+    NotImplementedSnafu.fail()
+}
