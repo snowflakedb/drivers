c2ba4205cc4f6db77f97d5439f8db2e3789c970b
Piotr Bulawa <piotr.bulawa@snowflake.com>
2026-03-25 12:17:38 +0100

    Add buffer check

--- odbc_tests/tests/e2e/types/binary_conversion_to_c_char.cpp
+++ odbc_tests/tests/e2e/types/binary_conversion_to_c_char.cpp
@@ -327,6 +327,8 @@ TEST_CASE("should truncate binary hex with one-byte-short buffer for SQL_C_CHAR"
   REQUIRE(ret == SQL_SUCCESS_WITH_INFO);
   REQUIRE(get_sqlstate(stmt) == "01004");
   REQUIRE(indicator == 6);
+  REQUIRE(std::string(buffer, 5) == "ABCDE");
+  REQUIRE(buffer[5] == '\0');
 }
 
 // ============================================================================
