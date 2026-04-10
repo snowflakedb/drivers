ef665ccaf1467375258af8b12ce073e37e39c2c1
Patryk Czajka <patryk.czajka@snowflake.com>
2026-04-10 12:35:19 +0200

    update tests

--- odbc_tests/tests/e2e/put_get/put_get_sse_stage.cpp
+++ odbc_tests/tests/e2e/put_get/put_get_sse_stage.cpp
@@ -17,64 +17,66 @@ static std::string create_sse_stage(Connection& conn, const std::string& stage_n
   return stage_name;
 }
 
-TEST_CASE("should put file to SSE stage", "[put_get]") {
+TEST_CASE("should put and get file on SSE stage", "[put_get]") {
   // Given Stage with server-side encryption (SNOWFLAKE_SSE)
   Connection conn;
-  const std::string stage = create_sse_stage(conn, unique_stage_name("ODBCTST_SSE_PUT"));
+  const std::string stage = create_sse_stage(conn, unique_stage_name("ODBCTST_SSE_PUT_GET"));
 
-  TempTestDir temp_dir("odbc_sse_put_");
-  fs::path test_file = write_text_file(temp_dir.path(), "sse_test.txt", "hello sse\n");
+  TempTestDir upload_dir("odbc_sse_upload_");
+  fs::path test_file = write_text_file(upload_dir.path(), "sse_test.txt", "hello sse\n");
 
   // When File is uploaded using PUT command
   std::string put_sql = "PUT 'file://" + as_file_uri(test_file) + "' @" + stage + " AUTO_COMPRESS=FALSE OVERWRITE=TRUE";
-  auto stmt = conn.execute_fetch(put_sql);
+  auto put_stmt = conn.execute_fetch(put_sql);
 
   // Then File should be uploaded successfully
-  CHECK(get_data<SQL_C_CHAR>(stmt, PUT_ROW_STATUS_IDX) == "UPLOADED");
-}
-
-TEST_CASE("should get file from SSE stage", "[put_get]") {
-  // Given File is uploaded to stage with server-side encryption (SNOWFLAKE_SSE)
-  Connection conn;
-  const std::string stage = create_sse_stage(conn, unique_stage_name("ODBCTST_SSE_GET"));
-
-  TempTestDir upload_dir("odbc_sse_upload_");
-  fs::path test_file = write_text_file(upload_dir.path(), "sse_get.txt", "get sse\n");
-
-  auto put_stmt = conn.execute_fetch("PUT 'file://" + as_file_uri(test_file) + "' @" + stage +
-                                     " AUTO_COMPRESS=FALSE OVERWRITE=TRUE");
-  REQUIRE(get_data<SQL_C_CHAR>(put_stmt, PUT_ROW_STATUS_IDX) == "UPLOADED");
+  CHECK(get_data<SQL_C_CHAR>(put_stmt, PUT_ROW_STATUS_IDX) == "UPLOADED");
 
   // When File is downloaded using GET command
   TempTestDir download_dir("odbc_sse_get_");
-  std::string get_sql = "GET @" + stage + "/sse_get.txt 'file://" + as_file_uri(download_dir.path()) + "/'";
+  std::string get_sql = "GET @" + stage + "/sse_test.txt 'file://" + as_file_uri(download_dir.path()) + "/'";
   auto get_stmt = conn.execute_fetch(get_sql);
 
   // Then File should be downloaded
   CHECK(get_data<SQL_C_CHAR>(get_stmt, GET_ROW_STATUS_IDX) == "DOWNLOADED");
 
   // And Have correct content
-  fs::path downloaded = download_dir.path() / "sse_get.txt";
+  fs::path downloaded = download_dir.path() / "sse_test.txt";
   REQUIRE(fs::exists(downloaded));
   std::ifstream ifs(downloaded);
   std::string content((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-  CHECK(content == "get sse\n");
+  CHECK(content == "hello sse\n");
 }
 
-TEST_CASE("should put file to SSE stage with DIRECTORY enabled", "[put_get]") {
+TEST_CASE("should put and get file on SSE stage with DIRECTORY enabled", "[put_get]") {
   // Given Stage with server-side encryption and DIRECTORY enabled
   Connection conn;
   const std::string stage = unique_stage_name("ODBCTST_SSE_DIR");
   conn.execute("CREATE TEMPORARY STAGE IF NOT EXISTS " + stage +
                " ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE') DIRECTORY = (ENABLE = TRUE)");
 
-  TempTestDir temp_dir("odbc_sse_dir_");
-  fs::path test_file = write_text_file(temp_dir.path(), "test.txt", "Initial contents\n");
+  TempTestDir upload_dir("odbc_sse_dir_");
+  fs::path test_file = write_text_file(upload_dir.path(), "test.txt", "Initial contents\n");
 
   // When File is uploaded using PUT command
   std::string put_sql = "PUT 'file://" + as_file_uri(test_file) + "' @" + stage + " AUTO_COMPRESS=FALSE OVERWRITE=TRUE";
-  auto stmt = conn.execute_fetch(put_sql);
+  auto put_stmt = conn.execute_fetch(put_sql);
 
   // Then File should be uploaded successfully
-  CHECK(get_data<SQL_C_CHAR>(stmt, PUT_ROW_STATUS_IDX) == "UPLOADED");
+  CHECK(get_data<SQL_C_CHAR>(put_stmt, PUT_ROW_STATUS_IDX) == "UPLOADED");
+
+  // When File is downloaded using GET command
+  TempTestDir download_dir("odbc_sse_dir_get_");
+  std::string get_sql = "GET @" + stage + "/test.txt 'file://" + as_file_uri(download_dir.path()) + "/'";
+  auto get_stmt = conn.execute_fetch(get_sql);
+
+  // Then File should be downloaded
+  CHECK(get_data<SQL_C_CHAR>(get_stmt, GET_ROW_STATUS_IDX) == "DOWNLOADED");
+
+  // And Have correct content
+  fs::path downloaded = download_dir.path() / "test.txt";
+  REQUIRE(fs::exists(downloaded));
+  std::ifstream ifs(downloaded);
+  std::string content((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+  CHECK(content == "Initial contents\n");
 }
