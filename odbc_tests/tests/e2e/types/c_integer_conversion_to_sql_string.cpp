4c6707ddd78f0c8a7c444296d0460adc4f86349a
Maxymilian Kowalski <maxymilian.kowalski@snowflake.com>
2026-04-14 13:30:18 +0200

    Add e2e bindparam tests: c_integer → sql_string, sql_boolean (#835)
    ## Summary
    
    - Adds `c_integer_conversion_to_sql_string` e2e test suite (10 test
    cases) covering all integer C types (SLONG, SBIGINT, SSHORT, UTINYINT,
    ULONG, STINYINT, USHORT, UBIGINT) bound to SQL_VARCHAR via
    SQLBindParameter
    - Adds `c_integer_conversion_to_sql_boolean` e2e test suite (10 test
    cases) covering the same integer C types bound to SQL_BIT
    
    These conversions already pass in the conversion matrix but lacked e2e
    test suites, causing ~70 cells to appear as "untested" on the ODBC
    report summary page.
    
    ## Test plan
    
    - [x] CI passes (pre-commit, format validator)
    - [x] e2e tests pass against Snowflake
    
    
    Made with [Cursor](https://cursor.com)

---
+++ odbc_tests/tests/e2e/types/c_integer_conversion_to_sql_string.cpp
@@ -0,0 +1,230 @@
+#include <limits>
+#include <string>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "Connection.hpp"
+#include "Schema.hpp"
+#include "get_data.hpp"
+#include "odbc_cast.hpp"
+#include "odbc_matchers.hpp"
+
+TEST_CASE("should bind SQL_C_SLONG to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_SLONG 42 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLINTEGER val = 42;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "42"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "42");
+}
+
+TEST_CASE("should bind SQL_C_SBIGINT to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_SBIGINT 9999999999 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLBIGINT val = 9999999999LL;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_SBIGINT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "9999999999"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "9999999999");
+}
+
+TEST_CASE("should bind SQL_C_SSHORT to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_SSHORT -32768 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLSMALLINT val = -32768;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_SSHORT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "-32768"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "-32768");
+}
+
+TEST_CASE("should bind SQL_C_UTINYINT to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_UTINYINT 255 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLCHAR val = 255;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_UTINYINT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "255"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "255");
+}
+
+TEST_CASE("should bind SQL_C_ULONG to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_ULONG 4000000000 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLUINTEGER val = 4000000000U;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_ULONG, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "4000000000"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "4000000000");
+}
+
+TEST_CASE("should bind SQL_C_STINYINT negative to SQL_VARCHAR", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_STINYINT -128 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLSCHAR val = -128;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_STINYINT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "-128"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "-128");
+}
+
+TEST_CASE("should bind SQL_C_USHORT to SQL_VARCHAR and read back", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_USHORT 65535 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLUSMALLINT val = 65535;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_USHORT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "65535"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "65535");
+}
+
+TEST_CASE("should bind SQL_C_UBIGINT max to SQL_VARCHAR", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_UBIGINT max is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLUBIGINT val = std::numeric_limits<SQLUBIGINT>::max();
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_UBIGINT, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back correctly
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == std::to_string(std::numeric_limits<SQLUBIGINT>::max()));
+}
+
+TEST_CASE("should bind SQL_C_SLONG zero to SQL_VARCHAR", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_SLONG 0 is bound to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLINTEGER val = 0;
+  SQLLEN ind = 0;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the value is read back as "0"
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data<SQL_C_CHAR>(fetch_stmt, 1) == "0");
+}
+
+TEST_CASE("should bind SQL_C_SLONG with NULL indicator to SQL_VARCHAR", "[c_integer][conversion][sql_string]") {
+  // Given Snowflake client is logged in
+  Connection conn;
+  auto random_schema = Schema::use_random_schema(conn);
+  conn.execute("CREATE TABLE t (col VARCHAR(100))");
+
+  // When SQL_C_SLONG is bound with SQL_NULL_DATA to SQL_VARCHAR and inserted
+  auto stmt = conn.createStatement();
+  SQLRETURN ret = SQLPrepare(stmt.getHandle(), sqlchar("INSERT INTO t VALUES (?)"), SQL_NTS);
+  REQUIRE_ODBC(ret, stmt);
+  SQLINTEGER val = 0;
+  SQLLEN ind = SQL_NULL_DATA;
+  ret = SQLBindParameter(stmt.getHandle(), 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_VARCHAR, 100, 0, &val, 0, &ind);
+  REQUIRE_ODBC(ret, stmt);
+  ret = SQLExecute(stmt.getHandle());
+  REQUIRE_ODBC(ret, stmt);
+
+  // Then the stored value should be NULL
+  auto fetch_stmt = conn.execute_fetch("SELECT col FROM t");
+  CHECK(get_data_optional<SQL_C_CHAR>(fetch_stmt, 1) == std::nullopt);
+}
