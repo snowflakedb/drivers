b1524a833992e6254ff83e07475c90ceb55bfa93
Michał Hofman <michal.hofman@snowflake.com>
2026-05-05 13:55:53 +0200

    log jdbc and python version at startup (#1025)

--- jdbc/src/main/java/net/snowflake/client/internal/unicore/JNICoreTransport.java
+++ jdbc/src/main/java/net/snowflake/client/internal/unicore/JNICoreTransport.java
@@ -1,5 +1,6 @@
 package net.snowflake.client.internal.unicore;
 
+import net.snowflake.client.api.driver.SnowflakeDriver;
 import net.snowflake.client.internal.log.SFLogger;
 import net.snowflake.client.internal.log.SFLoggerFactory;
 
@@ -31,6 +32,7 @@ public class JNICoreTransport implements CoreTransport {
         throw new RuntimeException("Failed to load jdbc native library", e2);
       }
     }
+    logger.info("JDBC driver starting v{}", SnowflakeDriver.getDriverVersion());
   }
 
   @Override
--- python/src/snowflake/connector/_internal/api_client/__init__.py
+++ python/src/snowflake/connector/_internal/api_client/__init__.py
@@ -1,6 +1,13 @@
 """Initialize the core API with the default logger callback."""
 
+import logging
+
 from .c_api import c_logger_callback, sf_core_init
 
 
 sf_core_init(c_logger_callback)
+
+from snowflake.connector.version import __version__  # noqa: E402
+
+
+logging.getLogger("snowflake.connector").info("Python connector starting v%s", __version__)
