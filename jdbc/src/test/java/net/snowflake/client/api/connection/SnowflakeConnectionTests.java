ab4d70a269060c82dd38b2dff2dcd863f6f8227d
Antoni Solarski <antoni.solarski@snowflake.com>
2026-07-08 09:35:47 +0200

    SNOW-3199607: jdbc - getSessionID
    GitOrigin-RevId: 8f8a5b8fd47799356ce1a8bf686e3973aa52051b

--- jdbc/src/test/java/net/snowflake/client/api/connection/SnowflakeConnectionTests.java
+++ jdbc/src/test/java/net/snowflake/client/api/connection/SnowflakeConnectionTests.java
@@ -1,17 +1,28 @@
 package net.snowflake.client.api.connection;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import net.snowflake.jdbc.utils.SnowflakeIntegrationTestBase;
 import org.junit.jupiter.api.Test;
 
 class SnowflakeConnectionTests extends SnowflakeIntegrationTestBase {
 
   @Test
-  void isValidReturnsTrueOnOpenConnection() throws Exception {
+  void isValidReturnsTrueOnOpenConnection() throws SQLException {
     try (Connection conn = openConnection()) {
       assertTrue(conn.isValid(0));
+    }
+  }
+
+  @Test
+  void shouldReturnSessionIdOnOpenConnection() throws SQLException {
+    try (Connection conn = openConnection()) {
+      String sessionID = conn.unwrap(SnowflakeConnection.class).getSessionID();
+      assertNotNull(sessionID);
+      assertTrue(Long.parseLong(sessionID) > 0);
     }
   }
 }
