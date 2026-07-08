3b89dce47f9d28c1e25058ea853778a36c99562e
Antoni Solarski <antoni.solarski@snowflake.com>
2026-07-08 09:22:42 +0200

    SNOW-3199607: jdbc - connection methods for creating statements
    GitOrigin-RevId: 6ea2d779784e2b38581959e4d2066793c20c9026

--- jdbc/src/main/java/net/snowflake/client/api/exception/ErrorCode.java
+++ jdbc/src/main/java/net/snowflake/client/api/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
       200038, null, "Cannot convert value in the driver from type:{0} to type:{1}, value={2}."),
   COLUMN_DOES_NOT_EXIST(200032, "22000", null),
   RESULTSET_ALREADY_CLOSED(200037, "0A000", null),
-  ARRAY_BIND_MIXED_TYPES_NOT_SUPPORTED(200023, "0A000", null);
+  ARRAY_BIND_MIXED_TYPES_NOT_SUPPORTED(200023, "0A000", null),
+  FEATURE_UNSUPPORTED(200035, "0A000", null);
 
   private final int messageCode;
   private final String sqlState;
