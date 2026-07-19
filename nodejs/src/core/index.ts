2dfe94ec7c68bdeef2f77366df231d2481affce6
Ruslan Savenok <ruslan.savenok@snowflake.com>
2026-07-19 16:49:52 +0200

    Node.js - prod-like napi build (#630)
    We want local development to work with the same packages that will be
    published on npm, so instead of importing from `_build` folder, we
    should use `npm link`
    * include package.json in `_build/snowflake-sdk-core/` folder (needed
    for linking)
    * instead of importing from `_build`, use npm link so the local
    development imports exactly the same package that we'll be publishing on
    npm

--- nodejs/src/core/index.ts
+++ nodejs/src/core/index.ts
@@ -1,5 +1,4 @@
 import { createRequire } from 'node:module';
-import type * as NativeCore from '../../_build/snowflake-sdk-core/index.d.ts';
 
 const require = createRequire(import.meta.url);
 
@@ -7,7 +6,7 @@ const require = createRequire(import.meta.url);
 // This is just placeholder file to test if binary compiled correctly
 // There will be a separate PR implementing proper platform-specific loading logic
 const core =
-  require('../../_build/snowflake-sdk-core/snowflake-sdk-core.darwin-arm64.node') as typeof NativeCore;
+  require('snowflake-sdk-core-darwin-arm64') as typeof import('snowflake-sdk-core-darwin-arm64');
 
 // oxlint-disable-next-line no-console
 console.log('core', core.dummyTestEntrypoint());
