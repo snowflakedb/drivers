775c1f95bd2e7ea4996df2171effcd3577c23fd7
Ruslan Savenok <ruslan.savenok@snowflake.com>
2026-07-19 23:18:12 +0200

    Node.js - Change napi build so typescript definitions land in src/core
    Binary TS definitions need to be stored in snowflake-sdk package,
    otherwise it's codebase won't be able to properly load binary types
    because they will come from different packages (if/else block) resulting
    in `any` return type
    
    GitOrigin-RevId: ff906b7df454ce8e073640f73bef83c17111f7d2

--- nodejs/src/core/index.ts
+++ nodejs/src/core/index.ts
@@ -1,12 +1,16 @@
 import { createRequire } from 'node:module';
+import type * as CoreBinary from './binary-types.generated.js';
 
 const require = createRequire(import.meta.url);
 
 // NOTE:
 // This is just placeholder file to test if binary compiled correctly
-// There will be a separate PR implementing proper platform-specific loading logic
-const core =
-  require('snowflake-sdk-core-darwin-arm64') as typeof import('snowflake-sdk-core-darwin-arm64');
+// There will be a separate PR implementing proper platform-specific if/else loading logic
+function getCore(): typeof CoreBinary {
+  return require('snowflake-sdk-core-darwin-arm64');
+}
+
+const core = getCore();
 
 // oxlint-disable-next-line no-console
 console.log('core', core.dummyTestEntrypoint());
