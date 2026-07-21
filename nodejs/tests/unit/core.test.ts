ae191bb6f7b0a17720f4b9f7976401779fc68e93
Ruslan Savenok <ruslan.savenok@snowflake.com>
2026-07-21 14:24:39 +0200

    [Node] Implement bare minimum for connect() and connectAsync() methods
    Bare minimum code that is able to connect via sf_core
    
    GitOrigin-RevId: c26599747e1c9541867f462ac224e2ce9894ba61

--- nodejs/tests/unit/core.test.ts
+++ nodejs/tests/unit/core.test.ts
@@ -1,6 +1,9 @@
 import { it, expect } from 'vitest';
 import core from '../../src/core/index.js';
 
-it('able to execute code from core binary', () => {
-  expect(core.dummyTestEntrypoint()).toBe('nodejs_bridge 0.0.1 ok');
+// TODO:
+// This is a placeholder test to verify that the core module compiles correctly.
+// Replace or remove as proper unit tests are implemented.
+it('loads core binary', () => {
+  expect(core.Connection).toBeTypeOf('function');
 });
