af9ab99db9089434734ff7d45a15084cdebb4df6
Ruslan Savenok <ruslan.savenok@snowflake.com>
2026-08-10 17:41:45 +0200

    [Node] prototype streamRows + fetchRows (#1016)
    Prototype fetchRows and streamRows methods
    
    This is not final implementation, just a way to test that those methods
    work

--- nodejs/src/query-result/RowStatement.ts
+++ nodejs/src/query-result/RowStatement.ts
@@ -1,11 +1,26 @@
+import type { Readable } from 'node:stream';
 import type { CoreStatementInstance } from '../core/index.js';
 import type { SnowflakeError } from '../error.js';
+import { createRowStream } from './rows.js';
 
 export type StatementCallback = (
   err: SnowflakeError | undefined,
   stmt: RowStatement | FileAndStageBindStatement,
   rows: Array<unknown> | undefined,
 ) => void;
+
+export type DataType = 'String' | 'Boolean' | 'Number' | 'Date' | 'JSON' | 'Buffer';
+
+export interface StreamOptions {
+  start?: number;
+  end?: number;
+  fetchAsString?: DataType[];
+}
+
+export interface FetchRowsOptions {
+  each: (row: unknown) => boolean | void;
+  end: (err: SnowflakeError | undefined, stmt: RowStatement | FileAndStageBindStatement) => void;
+}
 
 // TODO:
 // Not sure if we actually need to have a wrapper around core statement.
@@ -25,6 +40,38 @@ export class RowStatement {
 
   getQueryId(): string | undefined {
     return this.#core.getQueryId() ?? undefined;
+  }
+
+  // TODO: decide how to handle a case where user didn't set the streamResult: true
+  // and the result is already drained. (would suggest a BCR with error)
+  // oxlint-disable-next-line no-unused-vars
+  streamRows(options?: StreamOptions): Readable {
+    return createRowStream(this.#core);
+  }
+
+  fetchRows(options: FetchRowsOptions): void {
+    const stream = createRowStream(this.#core);
+    let finished = false;
+
+    const onComplete = (err: SnowflakeError | undefined) => {
+      if (finished) {
+        return;
+      }
+      finished = true;
+      options.end(err, this);
+    };
+
+    stream.on('data', (row: unknown) => {
+      if (finished) {
+        return;
+      }
+      if (options.each(row) === false) {
+        stream.destroy();
+        onComplete(undefined);
+      }
+    });
+    stream.on('end', () => onComplete(undefined));
+    stream.on('error', (err: Error) => onComplete(err as SnowflakeError));
   }
 
   cancel(callback?: StatementCallback): void {
--- nodejs/src/query-result/rows.ts
+++ nodejs/src/query-result/rows.ts
@@ -1,3 +1,4 @@
+import { Readable } from 'node:stream';
 import type { CoreStatementInstance } from '../core/index.js';
 
 export async function collectRows(coreStatement: CoreStatementInstance): Promise<unknown[]> {
@@ -14,4 +15,24 @@ export async function collectRows(coreStatement: CoreStatementInstance): Promise
   } finally {
     coreStatement.close();
   }
+}
+
+export function createRowStream(coreStatement: CoreStatementInstance): Readable {
+  return new Readable({
+    objectMode: true,
+    read() {
+      coreStatement
+        .getNextRow()
+        .then((row) => {
+          this.push(row);
+        })
+        .catch((err: Error) => {
+          this.destroy(err);
+        });
+    },
+    destroy(err, callback) {
+      coreStatement.close();
+      callback(err);
+    },
+  });
 }
